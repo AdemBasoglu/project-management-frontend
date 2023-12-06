b6f47fdb0e8df794a75c2c7b123a4b7edc7c5c1c
Adem Basoglu <admbasoglu28@gmail.com>
2023-12-06 22:20:52 +0100

    add-project-dialog-component

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {CommonModule} from '@angular/common';
 import {UserService} from "../../services/user.service";
 import {ProjectService} from "../../services/project.service";
 import {HomeComponent} from "../home/home.component";
@@ -8,6 +8,8 @@ import {User} from "../../interfaces/User";
 import {Router, RouterLink} from "@angular/router";
 import {AuthenticationService} from "../../services/authentication.service";
 import {MatButtonModule} from "@angular/material/button";
+import {MatDialog} from '@angular/material/dialog';
+import {AddProjectDialogComponent} from '../add-project-dialog/add-project-dialog.component'; // Ek bir bileşen olarak oluşturmanız gerekir
 
 @Component({
   selector: 'app-sidebar',
@@ -21,26 +23,16 @@ export class SidebarComponent implements OnInit {
   userEmail: string = '';
 
   project: Project = {
-    id: 0,
-    name: '',
+    id: 0, name: '',
   };
 
   sessionUser: User = {
-    email: '',
-    password: '',
-    firstName: '',
-    lastName: '',
-    projects: [],
-    tasks: [],
+    email: '', password: '', firstName: '', lastName: '', projects: [], tasks: [],
   };
 
-  constructor(
-    private userService: UserService,
-    private projectService: ProjectService,
-    private authentication:AuthenticationService,
-    private router:Router
-
-  ) {}
+  constructor(private userService: UserService, private projectService: ProjectService, private authentication: AuthenticationService, private router: Router, private dialog: MatDialog // MatDialog'ı buraya ekleyin
+  ) {
+  }
 
   ngOnInit(): void {
     console.log('home initialized');
@@ -50,24 +42,27 @@ export class SidebarComponent implements OnInit {
 
     this.userService.getUserByEmail(this.userEmail).subscribe({
       next: (user) => {
-        (this.sessionUser = user),
-          console.log(this.sessionUser),
-          (this.project = this.sessionUser.projects[0]);
-      },
-      error: (err) => console.log(err),
+        (this.sessionUser = user), console.log(this.sessionUser), (this.project = this.sessionUser.projects[0]);
+      }, error: (err) => console.log(err),
     });
 
     this.projectService.getProjectById(3).subscribe({
       next: (project) => {
         this.project = project;
-      },
-      error: (err) => console.log(err),
+      }, error: (err) => console.log(err),
     });
   }
 
 
-  addProject(){
-    this.projectService.addProject(this.project.name,this.userEmail)
+  addProject() {
+    const dialogRef = this.dialog.open(AddProjectDialogComponent);
+
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result) {
+        // Dialogdan gelen proje adını kullanarak yeni proje ekleme işlemleri
+        this.projectService.addProject(result, this.userEmail);
+      }
+    });
   }
 
   logout() {
