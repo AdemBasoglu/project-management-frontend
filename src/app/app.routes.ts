d039aab70050ef01c07e3d28cc8a64349a95689b
Maxim Baraliuc <m.baraliuc@gmail.com>
2023-12-06 11:11:18 +0100

    [upd] add proper routing for users project-list

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,7 @@ import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { ProjectComponent } from './components/project/project.component';
 import { ProjectListComponent } from './components/project-list/project-list.component';
-// import {BoardEditComponent} from "./components/board-edit/board-edit.component";
+import { BoardEditComponent } from './components/board-edit/board-edit.component';
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },
@@ -12,7 +12,7 @@ export const routes: Routes = [
   { path: 'register', component: RegisterComponent },
   { path: 'projects', component: ProjectListComponent },
   { path: 'project/:id', component: ProjectComponent },
-  // { path: 'board-edit', component: BoardEditComponent },
+  { path: 'board-edit', component: BoardEditComponent },
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: '**', component: HomeComponent },
 ];
--- src/app/components/project-list/project-list.component.ts
+++ src/app/components/project-list/project-list.component.ts
@@ -1,13 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Project } from '../../interfaces/Project';
+import { User } from '../../interfaces/User';
+import { ProjectService } from '../../services/project.service';
+import { UserService } from '../../services/user.service';
+import { RouterModule } from '@angular/router';
 
 @Component({
-  selector: 'app-project-list',
+  selector: 'pm-project-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './project-list.component.html',
-  styleUrl: './project-list.component.css'
+  styleUrl: './project-list.component.css',
 })
-export class ProjectListComponent {
+export class ProjectListComponent implements OnInit {
+  projects: Project[] = [];
+  sessionUser: User = {
+    email: '',
+    password: '',
+    firstName: '',
+    lastName: '',
+    projects: [],
+    tasks: [],
+  };
 
+  constructor(
+    private userService: UserService,
+    private projectService: ProjectService
+  ) {}
+
+  ngOnInit(): void {
+    const email = localStorage.getItem('email');
+
+    if (email) {
+      this.userService.getUserByEmail(email).subscribe({
+        next: (user) => {
+          (this.sessionUser = user),
+            console.log(this.sessionUser),
+            sessionStorage.setItem('user', JSON.stringify(this.sessionUser)),
+            (this.projects = this.sessionUser.projects);
+        },
+        error: (err) => console.log(err),
+      });
+    }
+  }
 }
